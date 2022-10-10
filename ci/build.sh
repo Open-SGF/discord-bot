f52a3895a71759cd9223fb515d7a2d33682579b7
Frederick Lawler <fred@fredlawl.com>
2022-10-10 18:50:17 -0500

    Specify prod.config.json settings than copy from example.config.json
    Releasing to prod means that we need to adjust the configuration ahead
    of time because on install, service automatically restarts. In order to
    not forget to update the example.config.json to use as prod config prior
    to release, create a prod.config.json instead to serve that purpose.

--- ci/build.sh
+++ ci/build.sh
@@ -4,7 +4,7 @@ BIN_DIR=$BUILD_DIR/opt/$PROJNAME/bin
 
 rm -rf $BUILD_DIR
 mkdir -p $BIN_DIR $SYSTEMD_DIR
-cp example.config.json $BIN_DIR/config.json
+cp prod.config.json $BIN_DIR/config.json
 
 go mod tidy
 CGO_ENABLED=0 go build -o $BIN_DIR/$PROJNAME main.go
