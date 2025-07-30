9ab078017167e42eb89e7941d3b18f7aaced665b
Levi Zitting <me@levizitting.com>
2025-07-30 03:34:12 -0500

    wip: discord notification service

--- pkg/worker/models.go
+++ pkg/worker/models.go
@@ -34,3 +34,22 @@ type MeetupImage struct {
 	BaseURL string `json:"baseUrl"`
 	Preview string `json:"preview"`
 }
+
+type DiscordRequest struct {
+	Embeds []DiscordEmbed `json:"embeds"`
+}
+
+type DiscordEmbed struct {
+	Title       string              `json:"title"`
+	Description string              `json:"description"`
+	URL         string              `json:"url"`
+	Timestamp   string              `json:"timestamp"`
+	Color       int                 `json:"color"`
+	Fields      []DiscordEmbedField `json:"fields"`
+}
+
+type DiscordEmbedField struct {
+	Name   string `json:"name"`
+	Value  string `json:"value"`
+	Inline bool   `json:"inline"`
+}
